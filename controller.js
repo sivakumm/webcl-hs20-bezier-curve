5546b67e8eca2312f996b8233aae8401e8c081cb
Mithusan Sivakumar <mithusan.sivakumar@siemens.com>
2020-10-30 21:29:46 +0100

    basic tool

---
+++ controller.js
@@ -0,0 +1,128 @@
+const selector   = document.querySelector('#version-selection');
+const checkTrace = document.querySelector('#trace-check');
+const checkLine  = document.querySelector('#line-check');
+const rangeInp   = document.querySelector('#range-state');
+const startBtn   = document.querySelector('#btn-start');
+const resetBtn   = document.querySelector('#btn-reset');
+
+startBtn.onclick    = () => start();
+resetBtn.onclick    = () => reset();
+checkTrace.onchange = () => renderCanvas();
+checkLine.onchange  = () => renderCanvas();
+rangeInp.oninput    = () => { movePercentage = rangeInp.value; setNextPointPosition(); renderCanvas(); };
+
+const framePointsLocations = [
+    { x: 100, y: 250 },
+    { x: 400, y: 50 },
+    { x: 500, y: 500 },
+    { x: 750, y: 200 },
+];
+
+let framePoints     = [];
+let linearPoints    = [];
+let quadraticPoints = [];
+let cubicPoints     = [];
+let bezierPoints    = [];
+
+let movePercentage  = 0;
+let interval        = null;
+let dragIdx         = -1;
+
+function reset() {
+    startBtn.removeAttribute('disabled');
+    selector.removeAttribute('disabled');
+    checkTrace.removeAttribute('disabled');
+    checkLine.removeAttribute('disabled');
+    rangeInp.removeAttribute('disabled');
+    clearInterval(interval);
+
+    movePercentage  = 0;
+    framePoints     = [];
+    linearPoints    = [];
+    quadraticPoints = [];
+    cubicPoints     = [];
+    bezierPoints    = [];
+
+    framePointsLocations.forEach( (p, idx) => {
+        framePoints.push({ x: p.x, y: p.y });
+        linearPoints.push({ x: p.x, y: p.y });
+        quadraticPoints.push({ x: p.x, y: p.y });
+        cubicPoints.push({ x: p.x, y: p.y });
+    });
+    linearPoints.pop();
+    quadraticPoints.pop();
+    quadraticPoints.pop();
+    cubicPoints.pop();
+    cubicPoints.pop();
+    cubicPoints.pop();
+
+    const traceState   = checkTrace.checked;
+    checkTrace.checked = false;
+    renderCanvas();
+
+    rangeInp.value     = movePercentage;
+    checkTrace.checked = traceState;
+}
+
+function start() {
+    startBtn.setAttribute('disabled', '');
+    selector.setAttribute('disabled', '');
+    checkTrace.setAttribute('disabled', '');
+    checkLine.setAttribute('disabled', '');
+    rangeInp.setAttribute('disabled', '');
+
+    interval = setInterval(() => {
+        setNextPointPosition();
+        renderCanvas();
+    }, 10);
+}
+
+function setNextPointPosition() {
+    if (movePercentage <= 1) {
+        calculateNextPosition(linearPoints, framePoints);
+        calculateNextPosition(quadraticPoints, linearPoints);
+        calculateNextPosition(cubicPoints, quadraticPoints);
+        bezierPoints.push(cubicPoints[0]);
+
+        movePercentage += 0.003;
+        rangeInp.value = movePercentage;
+    } else {
+        clearInterval(interval);
+    }
+}
+
+function calculateNextPosition(toCalculate, parent) {
+    for(let i = 0; i < toCalculate.length; i++) {
+        if (parent[i].x < parent[i + 1].x) {
+            toCalculate[i].x = parent[i].x + Math.abs(parent[i].x - parent[i + 1].x) * movePercentage;
+        } else {
+            toCalculate[i].x = parent[i].x - Math.abs(parent[i].x - parent[i + 1].x) * movePercentage;
+        }
+
+        if (parent[i].y < parent[i + 1].y) {
+            toCalculate[i].y = parent[i].y + Math.abs(parent[i].y - parent[i + 1].y) * movePercentage;
+        } else {
+            toCalculate[i].y = parent[i].y - Math.abs(parent[i].y - parent[i + 1].y) * movePercentage;
+        }
+    }
+}
+
+function mouseDownEvent(event, rect) {
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    dragIdx = framePointsLocations.findIndex(p => Math.abs(p.x - x) < 5 && Math.abs(p.y - y) < 5);
+}
+
+function mouseMoveEvent(event, rect) {
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    if (dragIdx >= 0) {
+        framePointsLocations[dragIdx].x = x;
+        framePointsLocations[dragIdx].y = y;
+        reset();
+    }
+}
+
+function mouseUpEvent() {
+    if (dragIdx >= 0) { dragIdx = -1; }
+}
---
+++ drawer.js
@@ -0,0 +1,66 @@
+const canvas = document.querySelector('canvas');
+canvas.style.border = '2px black solid';
+canvas.width = document.querySelector('.col').offsetWidth;
+canvas.height = 600;
+
+const ctx = canvas.getContext('2d');
+ctx.lineWidth = 2;
+
+function createPoint(point, color) {
+    ctx.beginPath();
+    ctx.ellipse(point.x, point.y, 5, 5, 0, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.strokeStyle = color;
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawLine(origin, destination, color) {
+    ctx.beginPath();
+    ctx.moveTo(origin.x, origin.y);
+    ctx.lineTo(destination.x, destination.y);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.closePath();
+}
+
+function renderCanvas() {
+    if (!checkTrace.checked) {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    for(let i = 1; i < framePoints.length; i++) {
+        const prev = framePoints[i - 1];
+        const curr = framePoints[i];
+        if (checkLine.checked) { drawLine(prev, curr, 'black'); }
+    }
+    
+    for (const point of framePoints) {
+        createPoint(point, 'blue');
+    }
+
+    for (let i = 0; i < Number(selector.value); i++) {
+        createPoint(linearPoints[i], 'red');
+        if (i > 0) {
+            if (checkLine.checked) { drawLine(linearPoints[i - 1], linearPoints[i], 'red'); }
+        }
+    }
+
+    for (let i = 0; i < (Number(selector.value) - 1); i++) {
+        createPoint(quadraticPoints[i], 'green');
+        if (i > 0) {
+            if (checkLine.checked) { drawLine(quadraticPoints[i - 1], quadraticPoints[i], 'green'); }
+        }
+    }
+
+    for (let i = 0; i < (Number(selector.value) - 2); i++) {
+        createPoint(cubicPoints[i], 'black');
+        if (i > 0) {
+            drawLine(cubicPoints[i - 1], cubicPoints[i], 'black');
+        }
+    }
+}
+
+canvas.addEventListener('mousedown', evt => mouseDownEvent(evt, canvas.getBoundingClientRect()));
+canvas.addEventListener('mousemove', evt => mouseMoveEvent(evt, canvas.getBoundingClientRect()));
+canvas.addEventListener('mouseup',   ()  => mouseUpEvent());
